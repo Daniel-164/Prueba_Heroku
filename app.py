43d0dd7951f2ad18a21251a2cf50df8e44b0ab11
Dani_PG <dapega01@gmail.com>
2021-04-22 13:07:06 +0200

    añadida pagina

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask, render_template, abort
+app = Flask(__name__)
 import os
-port=os.environ["PORT"]
-app.run('0.0.0.0',int(port), debug=True)
+
+@app.route('/')
+def pagina_principal():
+    return render_template("pagina.html")
+
+#port=os.environ["PORT"]
+#app.run('0.0.0.0',int(port), debug=True)
