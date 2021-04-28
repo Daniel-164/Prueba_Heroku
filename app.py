7025fb037faeba453302c188ab72e567f8556a98
Dani_PG <dapega01@gmail.com>
2021-04-28 19:16:37 +0200

    Pagina funfunciona pero no cambia valores

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ app = Flask(__name__)
 def pagina_principal():
     return render_template("pagina.html")
 
-@app.route('/libro/<int:isbn>')
+@app.route('/libro/<isbn>')
 def pagina_detalle_libro(isbn):
     with open("books.json") as fichero:
         doc = json.load(fichero)
     return render_template("detalle.html",isbn=doc[0]["isbn"],titulo=doc[0]["title"],imagen=doc[0]["thumbnailUrl"],numpaginas=doc[0]["pageCount"],descripcioncorta=doc[0]["shortDescription"],descripcionlarga=doc[0]["longDescription"],autores=doc[0]["authors"],categorias=doc[0]["categories"])
-
 app.run(debug=True)
